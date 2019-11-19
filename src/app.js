09d4314eb053577ce90992c1fb2f929914434828
Efrain Rivera <riveraefrain5@gmail.com>
2019-11-19 17:10:03 -0600

    Archivos innecesarios eliminados

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express, { json } from 'express';
 import morgan from 'morgan';
-import router from './routes/projects';
-import routerTask from './routes/tasks';
 import routerEmpresa from './routes/empresa';
 import routerProducto from './routes/productos';
 
@@ -12,8 +10,7 @@ app.use(morgan('dev'));
 app.use(json());
 
 //Importando rutas
-app.use('/api/projects', router);
-app.use('/api/tasks', routerTask);
+
 app.use('/api/empresas', routerEmpresa);
 app.use('/api/productos', routerProducto);
 
